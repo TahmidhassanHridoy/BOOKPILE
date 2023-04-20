ff3bb4f4304414a22b4af6fcdcfff1dcb08ee962
Tahmid Hassan ridoy <75554934+TahmidhassanHridoy@users.noreply.github.com>
2023-04-20 09:54:14 +0600

    release 1.0

--- Front-End/angular-ecommerce/src/app/services/product.service.ts
+++ Front-End/angular-ecommerce/src/app/services/product.service.ts
@@ -1,29 +1,31 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, map } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 import { Product } from '../common/product';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  getListProduct() {
-    throw new Error('Method not implemented.');
-  }
 
   private baseUrl = 'http://localhost:6060/api/products';
+
   constructor(private httpClient: HttpClient) { }
 
-  getProductList(): Observable<Product[]> {
-     return this.httpClient.get<GetResponse>(this.baseUrl)
-                .pipe(map(response => response._embedded.products)
-      );
-    }
+  getProductList(theCategoryId: number): Observable<Product[]> {
+
+    // need to build URL based on category id 
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
+
+    return this.httpClient.get<GetResponse>(searchUrl).pipe(
+      map(response => response._embedded.products)
+    );
+  }
 }
 
 interface GetResponse {
   _embedded: {
-    products:Product[];
+    products: Product[];
   }
 }
-  
